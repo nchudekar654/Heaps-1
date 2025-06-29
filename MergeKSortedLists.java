ee1a26b22deeac3b664046e22460e86fc5d1394d
nchudekar654 <nchudekar654@gmail.com>
2025-06-28 21:47:48 -0700

    Heaps-1 Completed

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -1,3 +1,7 @@
+//Time Complexity: O(log k) - heap insert, O(log k) - heap remove/poll, n- number of nodes 
+// O(n*log k)
+//Auxillary space - O(k)
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -29,5 +33,45 @@ class Solution {
 
         }
         return result.next;
+    }
+}
+
+//Solution: Merging list one by one
+//Time Comlpexity - O(k*n)
+//Space Complexity - O(1)
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode merged = new ListNode(Integer.MIN_VALUE);
+        for(ListNode head: lists){
+            if(head != null){
+                merged = merge(merged, head);
+            }
+        }
+        return merged.next;
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        while(p1 != null && p2!=null){
+            if(p1.val < p2.val){
+                curr.next = p1;
+                p1 = p1.next;
+            }
+            else{
+                curr.next = p2;
+                p2 = p2.next;
+            }
+            curr = curr.next;
+        }
+        if(p1 == null){
+            curr.next = p2;
+        }
+        if(p2 == null){
+            curr.next = p1;
+        }
+       return dummy.next; 
     }
 }
